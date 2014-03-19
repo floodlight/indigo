36831c0b38669a181422e368921d68a5bdcb81df
Rich Lane <rlane@bigswitch.com>
2014-03-19 11:18:00 -0700

    OFConnectionManager: always return a version from indigo_cxn_get_async_version
    Even if no controller is connected the packet-in still needs to go to the
    agents.
    
    It's clear now that we should replace indigo_core_packet_in, and similar
    functions, with interfaces that are version-independent.

--- modules/OFConnectionManager/module/src/ofconnectionmanager.c
+++ modules/OFConnectionManager/module/src/ofconnectionmanager.c
@@ -1820,7 +1820,10 @@ indigo_cxn_get_async_version(of_version_t* of_version)
         return INDIGO_ERROR_NONE;
     }
 
-    return INDIGO_ERROR_NOT_FOUND;
+    /* If there's no controller we still need to send the packet-in through the
+     * agents */
+    *of_version = OF_VERSION_1_3;
+    return INDIGO_ERROR_NONE;
 }
 
 /**
