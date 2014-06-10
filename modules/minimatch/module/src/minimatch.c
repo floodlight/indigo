3e7eef69e15de985c1607c718553b8f3eb33a1e7
Rich Lane <rlane@bigswitch.com>
2014-06-10 01:32:55 -0700

    minimatch: initial implementation
    Not optimized yet.

---
+++ modules/minimatch/module/src/minimatch.c
@@ -0,0 +1,143 @@
+/****************************************************************
+ *
+ *        Copyright 2014, Big Switch Networks, Inc.
+ *
+ * Licensed under the Eclipse Public License, Version 1.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *        http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ *
+ ****************************************************************/
+
+#include <minimatch/minimatch.h>
+#include <AIM/aim.h>
+#include <murmur/murmur.h>
+
+void
+minimatch_init(minimatch_t *minimatch, const of_match_t *match)
+{
+    int i;
+    int num_words = 0;
+    int idx = 0;
+    uint32_t *fields = (uint32_t *)&match->fields;
+    uint32_t *masks = (uint32_t *)&match->masks;
+
+    minimatch->version = match->version;
+    memset(minimatch->bitmap, 0, sizeof(minimatch->bitmap));
+
+    /*
+     * Find the nonzero match words and set their bit in the bitmap.
+     */
+    for (i = 0; i < OF_MATCH_FIELDS_WORDS; i++) {
+        if (masks[i]) {
+            num_words += 2;
+            minimatch->bitmap[i/32] |= (1 << (i % 32));
+        }
+    }
+
+    minimatch->num_words = num_words;
+    minimatch->words = aim_malloc(sizeof(uint32_t) * num_words);
+
+    /*
+     * For each bit set in the bitmap, copy the corresponding words from the
+     * fields and masks to our words array.
+     */
+    for (i = 0; i < OF_MATCH_FIELDS_WORDS; i++) {
+        if (masks[i]) {
+            minimatch->words[idx++] = fields[i];
+            minimatch->words[idx++] = masks[i];
+        }
+    }
+}
+
+void
+minimatch_cleanup(minimatch_t *minimatch)
+{
+    aim_free(minimatch->words);
+}
+
+void
+minimatch_expand(const minimatch_t *minimatch, of_match_t *match)
+{
+    int i;
+    int idx = 0;
+    uint32_t *fields = (uint32_t *)&match->fields;
+    uint32_t *masks = (uint32_t *)&match->masks;
+
+    memset(match, 0, sizeof(*match));
+    match->version = minimatch->version;
+
+    /*
+     * For each bit set in the bitmap, copy the corresponding words from the
+     * words array to the match fields and masks.
+     */
+    for (i = 0; i < OF_MATCH_FIELDS_WORDS; i++) {
+        uint32_t bitmap_word = minimatch->bitmap[i/32];
+        uint32_t bit = i % 32;
+        if ((bitmap_word >> bit) & 1) {
+            fields[i] = minimatch->words[idx++];
+            masks[i] = minimatch->words[idx++];
+        }
+    }
+}
+
+bool
+minimatch_equal(const minimatch_t *a, const minimatch_t *b)
+{
+    /* TODO optimize */
+    of_match_t match_a, match_b;
+    minimatch_expand(a, &match_a);
+    minimatch_expand(b, &match_b);
+    return of_match_eq(&match_a, &match_b);
+}
+
+bool
+minimatch_more_specific(const minimatch_t *a, const minimatch_t *b)
+{
+    /*
+     * For each match word, check that a's mask is at least as specific as b's
+     * mask and that a and b agree on the fields where b's mask is set.
+     */
+    /* TODO optimize */
+    of_match_t match_a, match_b;
+    minimatch_expand(a, &match_a);
+    minimatch_expand(b, &match_b);
+    return of_match_more_specific(&match_a, &match_b);
+}
+
+bool
+minimatch_overlap(const minimatch_t *a, const minimatch_t *b)
+{
+    /*
+     * For each match word, check that a and b agree on the fields where
+     * the intersection of the masks is set.
+     */
+    of_match_t match_a, match_b;
+    minimatch_expand(a, &match_a);
+    minimatch_expand(b, &match_b);
+    return of_match_overlap(&match_a, &match_b);
+}
+
+uint32_t
+minimatch_hash(const minimatch_t *minimatch, uint32_t seed)
+{
+    /* TODO optimize */
+    of_match_t match;
+    minimatch_expand(minimatch, &match);
+    return murmur_hash(&match, sizeof(match), seed);
+}
+
+void
+minimatch_move(minimatch_t *dst, minimatch_t *src)
+{
+    *dst = *src;
+    memset(src, 0, sizeof(*src));
+}
--- modules/minimatch/utest/main.c
+++ modules/minimatch/utest/main.c
@@ -17,9 +17,196 @@
  *
  ****************************************************************/
 
+#include <minimatch/minimatch.h>
+#include <AIM/aim.h>
+
+static void
+test_basic(void)
+{
+    minimatch_t a, b, c;
+    of_match_t match_tmp;
+
+    of_match_t match1 = {
+        .version = OF_VERSION_1_0,
+        .fields = {
+            .vlan_vid = 0x567,
+            .eth_dst = { { 0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc } },
+        },
+        .masks = {
+            .vlan_vid = 0xffff,
+            .eth_dst = { { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff } },
+        },
+    };
+
+    /* Same mask as match1, different fields */
+    of_match_t match2 = {
+        .version = OF_VERSION_1_0,
+        .fields = {
+            .vlan_vid = 0x567,
+            .eth_dst = { { 0x12, 0x34, 0x56, 0x78, 0x9a, 0xbd } },
+        },
+        .masks = {
+            .vlan_vid = 0xffff,
+            .eth_dst = { { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff } },
+        },
+    };
+
+    /* More specific version of match2 */
+    of_match_t match3 = {
+        .version = OF_VERSION_1_0,
+        .fields = {
+            .vlan_vid = 0x567,
+            .eth_dst = { { 0x12, 0x34, 0x56, 0x78, 0x9a, 0xbd } },
+            .eth_type = 0x0800,
+        },
+        .masks = {
+            .vlan_vid = 0xffff,
+            .eth_dst = { { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff } },
+            .eth_type = 0xffff,
+        },
+    };
+
+    /* Verify match more-specific */
+    AIM_ASSERT(of_match_more_specific(&match1, &match1));
+    AIM_ASSERT(!of_match_more_specific(&match1, &match2));
+    AIM_ASSERT(!of_match_more_specific(&match1, &match3));
+    AIM_ASSERT(!of_match_more_specific(&match2, &match1));
+    AIM_ASSERT(of_match_more_specific(&match2, &match2));
+    AIM_ASSERT(!of_match_more_specific(&match2, &match3));
+    AIM_ASSERT(!of_match_more_specific(&match3, &match1));
+    AIM_ASSERT(of_match_more_specific(&match3, &match2));
+    AIM_ASSERT(of_match_more_specific(&match3, &match3));
+
+    /* Verify match overlap */
+    AIM_ASSERT(of_match_overlap(&match1, &match1));
+    AIM_ASSERT(!of_match_overlap(&match1, &match2));
+    AIM_ASSERT(!of_match_overlap(&match1, &match3));
+    AIM_ASSERT(!of_match_overlap(&match2, &match1));
+    AIM_ASSERT(of_match_overlap(&match2, &match2));
+    AIM_ASSERT(of_match_overlap(&match2, &match3));
+    AIM_ASSERT(!of_match_overlap(&match3, &match1));
+    AIM_ASSERT(of_match_overlap(&match3, &match2));
+    AIM_ASSERT(of_match_overlap(&match3, &match3));
+
+    minimatch_init(&a, &match1);
+    minimatch_init(&b, &match2);
+    minimatch_init(&c, &match3);
+
+    /* Minimatches should expand to the original match */
+    minimatch_expand(&a, &match_tmp);
+    AIM_ASSERT(of_match_eq(&match1, &match_tmp), "");
+    minimatch_expand(&b, &match_tmp);
+    AIM_ASSERT(of_match_eq(&match2, &match_tmp), "");
+    minimatch_expand(&c, &match_tmp);
+    AIM_ASSERT(of_match_eq(&match3, &match_tmp), "");
+
+    /* Test equality */
+    AIM_ASSERT(minimatch_equal(&a, &a), "");
+    AIM_ASSERT(!minimatch_equal(&a, &b), "");
+    AIM_ASSERT(!minimatch_equal(&a, &c), "");
+    AIM_ASSERT(!minimatch_equal(&b, &a), "");
+    AIM_ASSERT(minimatch_equal(&b, &b), "");
+    AIM_ASSERT(!minimatch_equal(&b, &c), "");
+    AIM_ASSERT(!minimatch_equal(&c, &a), "");
+    AIM_ASSERT(!minimatch_equal(&c, &b), "");
+    AIM_ASSERT(minimatch_equal(&c, &c), "");
+
+    /* Test more-specific */
+    AIM_ASSERT(minimatch_more_specific(&a, &a), "");
+    AIM_ASSERT(!minimatch_more_specific(&a, &b), "");
+    AIM_ASSERT(!minimatch_more_specific(&a, &c), "");
+    AIM_ASSERT(!minimatch_more_specific(&b, &a), "");
+    AIM_ASSERT(minimatch_more_specific(&b, &b), "");
+    AIM_ASSERT(!minimatch_more_specific(&b, &c), "");
+    AIM_ASSERT(!minimatch_more_specific(&c, &a), "");
+    AIM_ASSERT(minimatch_more_specific(&c, &b), "");
+    AIM_ASSERT(minimatch_more_specific(&c, &c), "");
+
+    /* Test overlap */
+    AIM_ASSERT(minimatch_overlap(&a, &a), "");
+    AIM_ASSERT(!minimatch_overlap(&a, &b), "");
+    AIM_ASSERT(!minimatch_overlap(&a, &c), "");
+    AIM_ASSERT(!minimatch_overlap(&b, &a), "");
+    AIM_ASSERT(minimatch_overlap(&b, &b), "");
+    AIM_ASSERT(minimatch_overlap(&b, &c), "");
+    AIM_ASSERT(!minimatch_overlap(&c, &a), "");
+    AIM_ASSERT(minimatch_overlap(&c, &b), "");
+    AIM_ASSERT(minimatch_overlap(&c, &c), "");
+
+    minimatch_cleanup(&a);
+    minimatch_cleanup(&b);
+    minimatch_cleanup(&c);
+}
+
+void
+random_match(of_match_t *match)
+{
+    memset(match, 0, sizeof(*match));
+    match->version = OF_VERSION_1_3;
+
+    uint8_t *fields = (uint8_t *)&match->fields;
+    uint8_t *masks = (uint8_t *)&match->masks;
+
+    /* Use a very small portion of the match to ensure we get some
+     * equal/overlap/more-specific matches. */
+    const int match_bytes = 12;
+    AIM_ASSERT(match_bytes < sizeof(of_match_t));
+    AIM_ASSERT(match_bytes*2 <= 32);
+
+    uint32_t r = random();
+
+    int i;
+    for (i = 0; i < match_bytes; i++) {
+        uint8_t mask = r & 1;
+        r >>= 1;
+        uint8_t field = r & mask;
+        r >>= 1;
+        fields[i] = field;
+        masks[i] = mask;
+    }
+}
+
+void
+test_random(void)
+{
+    int i;
+    int collisions = 0;
+    const int num_iters = 1000000;
+
+    for (i = 0; i < num_iters; i++) {
+        of_match_t match_a, match_b, match_tmp;
+        minimatch_t minimatch_a, minimatch_b;
+
+        random_match(&match_a);
+        random_match(&match_b);
+
+        minimatch_init(&minimatch_a, &match_a);
+        minimatch_init(&minimatch_b, &match_b);
+
+        minimatch_expand(&minimatch_a, &match_tmp);
+        AIM_ASSERT(of_match_eq(&match_a, &match_tmp), "");
+
+        minimatch_expand(&minimatch_b, &match_tmp);
+        AIM_ASSERT(of_match_eq(&match_b, &match_tmp), "");
+
+        AIM_ASSERT(minimatch_equal(&minimatch_a, &minimatch_b) == of_match_eq(&match_a, &match_b));
+        AIM_ASSERT(minimatch_more_specific(&minimatch_a, &minimatch_b) == of_match_more_specific(&match_a, &match_b));
+        AIM_ASSERT(minimatch_overlap(&minimatch_a, &minimatch_b) == of_match_overlap(&match_a, &match_b));
+
+        if (minimatch_hash(&minimatch_a, 0) == minimatch_hash(&minimatch_b, 0)) {
+            if (!minimatch_equal(&minimatch_a, &minimatch_b)) {
+                collisions++;
+            }
+        }
+    }
+
+    AIM_ASSERT(collisions < num_iters/10000, "too many hash collisions (%d)", collisions);
+}
+
 int
 main(int argc, char* argv[])
 {
+    test_basic();
+    test_random();
     return 0;
 }
-
